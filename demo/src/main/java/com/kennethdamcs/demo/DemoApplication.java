67aec5ab609ee3637708c7bd4736f7b9488853fc
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-16 13:57:21 -0800

    Changes to StudentController and added StudentService

--- demo/src/main/java/com/kennethdamcs/demo/DemoApplication.java
+++ demo/src/main/java/com/kennethdamcs/demo/DemoApplication.java
@@ -3,8 +3,6 @@ package com.kennethdamcs.demo;
 import com.kennethdamcs.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Month;
